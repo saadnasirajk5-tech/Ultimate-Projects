25d54b2b45bedf8240b793a91c2b61a1eb207a4d
Saad Nasir <saadnasirajk5@gmail.com>
2026-03-11 20:21:03 +0500

    Checking if it runs

---
+++ Automated_Data_Ingestion_Script.py
@@ -0,0 +1,29 @@
+""" 
+A Python script that connects to a public API (e.g., GitHub, weather data), handles pagination,
+and writes the structured data to a local file.	Demonstrates proficiency in
+API interaction, error handling, and basic data serialization (JSON/CSV).	
+Python (requests), pydantic (for data validation)
+"""
+print("hello world")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
